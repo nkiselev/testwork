cc0653b297a6397e6ef8c917f43090bc1c1188ec
Nick Kiselev <nicholas-kiselev@yandex.ru>
2021-04-25 19:05:47 +0300

    Add swagger doc for API
    Look at this /api/doc

--- src/Controller/ServerApi/V1/MetalController.php
+++ src/Controller/ServerApi/V1/MetalController.php
@@ -4,6 +4,8 @@ namespace App\Controller\ServerApi\V1;
 
 use App\Dto\MetalDto;
 use App\Entity\Supplier;
+use Nelmio\ApiDocBundle\Annotation\Model;
+use OpenApi\Annotations as OA;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
@@ -24,6 +26,17 @@ class MetalController extends AbstractController
      * @Route("metal/{id}", name="server_api_v1_metal", methods={"GET"})
      * @param Supplier $supplier
      * @return JsonResponse
+     *
+     * @OA\Response(
+     *     response="200",
+     *     @OA\MediaType(
+     *         mediaType="application/json",
+     *         @OA\Schema(
+     *             ref=@Model(type=MetalDto::class)
+     *         )
+     *     ),
+     *     description="Return one random metal for supplier"
+     * )
      */
     public function show(Supplier $supplier): Response
     {
